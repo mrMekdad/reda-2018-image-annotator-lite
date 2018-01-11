e24c28e2860db30226a9c51aca6d3fd5accbf200
mrMekdad <mekdadr6@gmail.com>
2018-01-12 00:59:00 +0100

    image-annotator-lite: add repository metadata and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reda-2018-image-annotator-lite",
+    version="0.1.0",
+    description="Utility to catalog image batches, labels, and manual review notes.",
+    author="Red@",
+    packages=["image_annotator_lite"],
+    package_dir={"": "src"},
+)
